a2510537e14fb1502b0b868ec839f20049d3d39d
pkluiber <pkluiber@gmail.com>
2021-10-16 13:18:24 +0200

    FE21-CR6-Kluiber

--- Kluiber/src/app/app.module.ts
+++ Kluiber/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeadComponent } from './head/head.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardsComponent } from './cards/cards.component';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +11,6 @@ import { BlogComponent } from './blog/blog.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeadComponent,
     NavbarComponent,
     CardsComponent,
     HomeComponent,
